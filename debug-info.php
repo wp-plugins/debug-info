7e495289602ccf05f4796469bda5213a77264598
scott.deluzio <scott.deluzio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-07 16:59:36 +0000

    First commit of debug info
    git-svn-id: https://plugins.svn.wordpress.org/debug-info/trunk@853338 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ debug-info.php
@@ -0,0 +1,106 @@
+<?php
+   /*
+   Plugin Name: Debug Info
+   Plugin URI: http://oizuled.com/wordpress-plugins/wordpress-debug-info-plugin
+   Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=H2A9X5BC7P4MN
+   Description: A plugin to display your server's PHP info and WordPress environment data for debugging purposes.
+   Version: 1.0
+   Author: Scott DeLuzio
+   Author URI: http://oizuled.com
+   License: GPL2
+   */
+   
+	/*  Copyright 2013  Scott DeLuzio  (email : scott (at) oizuled.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	*/
+
+/* Add language support */
+function php_info_lang() {
+	load_plugin_textdomain('php_info_translate', false, dirname(plugin_basename(__FILE__)) . '/lang/');
+}
+add_action('init', 'php_info_lang');
+	
+/* Info Page */
+
+// Hook for adding admin menus
+add_action('admin_menu', 'oizuled_debug_info');
+
+// action function for above hook
+function oizuled_debug_info() {
+    // Add a new submenu under Settings:
+    add_dashboard_page('Debug Info','Debug Info', 'manage_options', 'oizuleddebuginfo', 'oizuled_debug_info_page');
+	
+}
+
+function oizuled_get_php_info() {
+	//retrieve php info for current server
+	ob_start();
+	phpinfo();
+	$pinfo = ob_get_contents();
+	ob_end_clean();
+ 
+	$pinfo = preg_replace( '%^.*<body>(.*)</body>.*$%ms','$1',$pinfo);
+	echo $pinfo;
+}
+
+function oizuled_version_check() {
+	//outputs basic information
+	$wp = get_bloginfo( 'version' );
+	$theme = wp_get_theme();
+	$plugins = get_option('active_plugins', array());
+	
+	$php = phpversion();
+	
+	$mem_usage = memory_get_usage(true);
+	if ($mem_usage < 1024) {
+        $phpmemuse = $mem_usage."B"; 
+    } elseif ($mem_usage < 1048576) {
+        $phpmemuse = round($mem_usage/1024,2)."K"; 
+    } else {
+        $phpmemuse = round($mem_usage/1048576,2)."M"; 
+	}
+	$phpmemlim = ini_get('memory_limit');
+	
+	$wpver = __('WordPress Version: ', 'php_info_translate');
+	$themever = __('Current WordPress Theme: ', 'php_info_translate');
+	$themeversion = $theme->get('Name') . __(' version ', 'php_info_translate') . $theme->get('Version') . $theme->get('Template');
+	$themeauthor = __(' Theme Author: ', 'php_info_translate');
+	$themeauth = $theme->get('Author') . ' - ' . $theme->get('AuthorURI');
+	$themeuri = __(' Theme URI: ', 'php_info_translate');
+	$uri = $theme->get('ThemeURI');
+	$pluginlist = __('Active Plugins: ', 'php_info_translate');
+	$phpver = __('PHP Version: ', 'php_info_translate');
+	$phpmemory = __('PHP Memory Usage: ', 'php_info_translate');
+	$outof = __(' out of ', 'php_info_translate');
+		
+	echo '<strong>' . $wpver . '</strong>' . $wp . '<br />';
+	echo '<strong>' . $themever . '</strong>' . $themeversion . '<br />';
+	echo '<strong>' . $themeauthor . '</strong>' . $themeauth . '<br />';
+	echo '<strong>' .  $themeuri . '</strong>' . $uri . '<br />'; 
+	echo '<strong>' . $pluginlist . '</strong>';
+		foreach ( $plugins as $plugin ) {
+			echo $plugin . ' | ';
+		}
+	echo '<br /><strong>' . $phpver . '</strong>' . $php . '<br />';
+	echo '<strong>' . $phpmemory . '</strong>' . $phpmemuse . $outof . $phpmemlim . '<br />';
+	
+}
+
+// Display the page content for the PHP Info submenu
+function oizuled_debug_info_page() {
+	include(WP_PLUGIN_DIR.'/debug-info/options.php');  
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,55 @@
+=== Debug Info ===
+Contributors: scott.deluzio
+Tags: database, php, memory, version, wordpress, admin, debug, plugin, theme
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=H2A9X5BC7P4MN
+Requires at least: 3.0
+Tested up to: 3.8
+Stable tag: 1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A plugin to display your server's PHP info and WordPress environment data for debugging purposes.
+
+== Description ==
+Debug Info will provide you with information on your current WordPress operating environment. This is a helpful tool for you or a developer to use when trying to identify the root cause of an error, or other issue you may experience.
+
+Debug Info provides the following information:
+* WordPress version currently running on the site
+* Current/active WordPress theme name, theme version, theme author, and theme URI
+* List of active plugins
+* PHP version
+* PHP memory usage and memory limit
+* Full phpinfo() server environment information 
+
+== Installation ==
+1. Download archive and unzip in wp-content/plugins or install via Plugins - Add New.
+2. Activate the plugin through the Plugins menu in WordPress.
+3. View debug information under Dashboard > Debug Info.
+
+== Frequently Asked Questions ==
+= Will this plugin fix the problems my site is having? =
+No, however it may help identify potential issues.
+
+= I don't understand most of the information this plugin produces. =
+If you need help fixing a problem, the information this plugin produces is helpful in order to identify compatibility issues between themes and plugins or different WordPress versions. PHP information is provided to help identify any potential server side conflicts.
+
+If you are still stuck, you can always <a href="http://oizuled.com/">ask for help</a>.
+
+= I installed the plugin, and it hasn't helped identify the problem. =
+This plugin may not identify every problem your site may face. 
+
+For example, if your site gets hacked, this plugin will not be able to identify that. However, it will be able to identify if you are using an outdated version of WordPress or a theme, which may indicate potential security holes.
+
+= Can I translate this plugin? =
+Sure! Once you have translated it, let me know and I'll be sure to get your translation included in the next update.
+
+== Screenshots ==
+1. Debug Info screenshot-1.png
+
+== Changelog ==
+= 1.0 =
+* Initial release.
+
+== Upgrade Notice ==
+= 1.0 =
+* Initial release.
